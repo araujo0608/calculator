b7247fe55dd30e59bd78e569f05514f3173dcfe0
araujo0608 <68195900+araujo0608@users.noreply.github.com>
2023-04-09 00:00:47 -0300

    Create basic structure
    Just creating the basics folders with default files

---
+++ src/components/Buttons/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles"; // Button StyleSheet
+
+export default function Buttons(){
+    return(
+        <View>
+            <Text>Button component</Text>
+        </View>
+    );
+}
---
+++ src/components/Display/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles"; // Display StyleSheet
+
+export default function Display(){
+    return(
+        <View>
+            <Text>Display component</Text>
+        </View>
+    );
+}
